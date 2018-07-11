ee3a7edbb6d7c1688c701239421ea3e882ed92ce
Ayshan Safarova <ayshans@crocusoft.com>
2018-07-11 12:03:59 +0400

    object-fit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import "bootstrap";
 import './main.scss';
 
 var a = "aaaa"
-document.getElementById("div1").innerHTML = "Hello World " + a;
+// document.getElementById("div1").innerHTML = "Hello World " + a;
